13a00a863b3bd9017da96a3e83448967b6dd3c05
Gaby Nieva <gaby.nieva@outlook.com>
2025-07-04 11:49:32 -0600

    Creating VentanaPrincipal

--- src/App.java
+++ src/App.java
@@ -1,10 +1,10 @@
-import javax.swing.JFrame;
+
+import screens.VentanaPrincipal;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        JFrame frame = new JFrame("Proyecto Escuela");
-        frame.setSize(500, 400);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+       VentanaPrincipal ventanaPrincipal = 
+       new VentanaPrincipal("Proyecto Escuela");
+       ventanaPrincipal.setVisible(true);
     }
 }
