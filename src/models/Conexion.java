1fcfedae873ec92eb23c31819a23eaeb2e3ce886
Gaby Nieva <gaby.nieva@outlook.com>
2025-07-16 13:17:07 -0600

    Conecting to a mysql db

---
+++ src/models/Conexion.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/escuela3a";
+    private static final String USER = "";
+    private static final String PASSWORD="";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
